82f3cd8de6f2783bbd2a4b16d3877d46a431febf
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-12-01 15:56:07 -0700

    Discard unused `Settings` instance after constructor

--- YetAnotherPriceTooltip.cs
+++ YetAnotherPriceTooltip.cs
@@ -8,12 +8,11 @@ namespace YetAnotherPriceTooltip;
 internal class YetAnotherPriceTooltip : BaseUnityPlugin {
     internal static new ManualLogSource Logger { get; private set; }
     public static new ConfigFile Config { get; private set; }
-    private static Settings Settings;
 
     private void Awake() {
         Logger = base.Logger;
         Config = base.Config;
-        Settings = new Settings(Config);
+        _ = new Settings(Config);
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         harmony.PatchAll();
         Logging.Log($"{MyPluginInfo.PLUGIN_NAME} version {MyPluginInfo.PLUGIN_VERSION} successfully loaded", LogLevel.Message);
